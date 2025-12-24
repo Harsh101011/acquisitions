fe9dc67b36625685b6032ca978262986a2ead0ca
Harsh Mahajan <harsh70mahajan@gmail.com>
2025-12-25 00:34:28 +0530

    Setup Neon Postgres with Drizzle

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Listening on ${PORT}...`);
-// })
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello from Acquisitions!');
